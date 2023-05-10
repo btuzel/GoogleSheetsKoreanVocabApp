9ece9d0444cd58456048b8989eeb61d5b107c007
betu <berkay.tuzel@zuhlke.com>
2023-05-11 00:37:18 +0100

    fix

--- app/src/main/java/com/example/googlesheetskoreanvocabapp/results/ResultsViewModel.kt
+++ app/src/main/java/com/example/googlesheetskoreanvocabapp/results/ResultsViewModel.kt
@@ -21,11 +21,29 @@ class ResultsViewModel @Inject constructor(private val getResult: GetResult) : V
 
     init {
         viewModelScope.launch {
-            _uiState.value = getResult()?.toList()?.let { SendResults(it) } ?: SendResults(listOf())
+            _uiState.value = getResult()?.toList()?.let {
+                getResult()?.let { it1 ->
+                    SendResults(
+                        sendResultsAccordingToDate(it1)
+                    )
+                }
+            } ?: SendResults(listOf())
         }
+    }
+
+    private fun sendResultsAccordingToDate(setOfStr: Set<String>): List<String> {
+        return setOfStr.toList().sortedByDescending { it.split("%")[1] }
     }
 
     data class SendResults(
         val data: List<String>
     )
+
+    fun splitString(str: String): List<String> {
+        return str.split("%")
+    }
 }
+
+//4%USEFUL_PHRASES%2023-05-08 23:29:47%1%2%thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다
+//4%USEFUL_PHRASES%2023-05-07 23:29:47%1%2%thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다
+//4%USEFUL_PHRASES%2023-05-03 23:29:47%1%2%thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다/thank you감사합니다
